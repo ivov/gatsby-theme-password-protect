386a57cadec107fb01147e4bfe5c419b3497e7c9
king children <dav92lee@gmail.com>
2020-06-27 11:06:15 -0400

    update styling for pw protect form

--- src/components/PasswordProtect.js
+++ src/components/PasswordProtect.js
@@ -7,7 +7,7 @@ import { setSessionPassword } from '../utils/utils';
 const styles = {
   wrapper: {
     height: '100vh',
-    background: '#2e8b57',
+    background: "white",
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
@@ -20,18 +20,18 @@ const styles = {
     fontSize: '20px',
     textAlign: 'center',
     border: '0',
-    borderRadius: '6px'
+    borderRadius: '2px'
   },
   button: {
     margin: 'auto',
     width: '70%',
     height: '48px',
-    background: 'rebeccapurple',
+    background: 'black',
     color: '#fff',
     fontWeight: 'bold',
     fontSize: '14px',
     border: '0',
-    borderRadius: '20px',
+    borderRadius: '2px',
     // borderRadius: '4px',
     marginTop: '30px',
     // textTransform: 'uppercase',
@@ -76,11 +76,6 @@ const PasswordProtect = () => {
         alignItems: 'center'
       }}
     >
-      <h1 style={{ color: '#fff', marginBottom: '0px', paddingBottom: '0px' }}>
-        Protected post
-      </h1>
-      <h4 style={{ color: '#fff' }}>Password?</h4>
-
       <form
         onSubmit={onSubmit}
         style={{
@@ -111,33 +106,6 @@ const PasswordProtect = () => {
           Enter!
         </button>
       </form>
-
-      {/* <a
-        href="https://gitlab.com/mkit/open-source/gatsby-theme-password-protect#readme"
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{
-          ...styles.link,
-          ...(isThemeHovered ? styles.linkHover : null)
-        }}
-        onMouseEnter={() => themeHover(true)}
-        onMouseLeave={() => themeHover(false)}
-      >
-        gatsby-theme-password-protect
-      </a> */}
-      {/* <a
-        href="https://mkit.io"
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{
-          ...styles.link,
-          ...(isSiteHovered ? styles.linkHover : null)
-        }}
-        onMouseEnter={() => siteHover(true)}
-        onMouseLeave={() => siteHover(false)}
-      >
-        MK IT
-      </a> */}
     </div>
   );
 };
